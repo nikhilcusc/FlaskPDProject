31a1982ccf25f4e96191cbc1428506770ba812a6
nikhilcusc <nikhilc@usc.edu>
2019-07-07 16:00:47 -0700

    trying to lines in linechart

--- Sol/websw.py
+++ Sol/websw.py
@@ -30,6 +30,18 @@ def chart():
 def charts2():
     labels = ["January","February","March","April","May","June","July","August","September","October","November","December"]
     values = [10,9,8,7,6,4,7,8,6,4,9,2]
-    return render_template('charts3.html',title='chart',labels= labels, values= values)
+    values2 = [9,8,7,6,4,7,8,6,4,9,2,10]
+    return render_template('charts3.html',title='chart',labels= labels, values= values, values2= values2)
 
+@app.route('/linechart')
+def linechart():
+    labels = [1-05-12, 30-04-12,
+27-04-12,26-04-12,
+25-04-12, 24-04-12,
+23-04-12,20-04-12,
+19-04-12, 18-04-12,
+17-04-12, 16-04-12,]
+    values = [10,9,8,7,6,4,7,8,6,4,9,2]
+    values2 = [9,8,7,6,4,7,8,6,4,9,2,10]
+    return render_template('linechart.html',title='chart',labels= labels, values= values, values2= values2)
 
