9a004c07c24a1b6e263421cb21e6781b2b84ff89
nikhilcusc <nikhilc@usc.edu>
2019-07-07 19:47:40 -0700

    imported data from xlsx sheet

--- Sol/websw.py
+++ Sol/websw.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, Markup
-#from forms import *
+import pandas as pd
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -35,13 +38,25 @@ def charts2():
 
 @app.route('/linechart')
 def linechart():
+    dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx') 
+    df = pd.DataFrame(dataxls)
+    df.fillna(0)
+    temperature = []
+    consumption = []
+    time = []
+    for iter1, iter2 in df.iterrows():
+        temperature.append(iter2.out_door_temp)
+        consumption.append(iter2.electricity_usage)
+        temptime = iter2.time 
+        temptime = temptime.ctime()
+        time.append(temptime) 
     labels = [1-05-12, 30-04-12,
 27-04-12,26-04-12,
 25-04-12, 24-04-12,
 23-04-12,20-04-12,
 19-04-12, 18-04-12,
-17-04-12, 16-04-12,]
+17-04-12, 16-04-12]
     values = [0,10,9,8,2,6,4,7,8,6,4,9]
     values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
-    return render_template('linechart.html',title='chart',labels= labels, values= values, values2= values2)
+    return render_template('linechart.html',temperature= temperature,consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
 
--- runFlask.sh
+++ runFlask.sh
@@ -1,3 +1,4 @@
+#pip install xlrd
 #pip install flask-wtf
 export FLASK_DEBUG=1
 export FLASK_APP=./Sol/websw.py
