ff2cb96f21c88697e8358e47a969f5a6497e85fb
nikhilcusc <nikhilc@usc.edu>
2019-07-27 16:13:26 -0700

    changing to chartjs now

--- Sol/websw.py
+++ Sol/websw.py
@@ -21,23 +21,57 @@ def signup():
 def login():
     #form = loginForm()
     return render_template('login.html',title='login')
-
-
-@app.route('/chart')
-def chart():
-    labels = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    values = [10,9,8,7,6,4,7,8,6,4,9,2]
-    return render_template('chart.html',title='chart',labels= labels, values= values)
-
-@app.route('/charts2')
-def charts2():
-    labels = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    values = [10,9,8,7,6,4,7,8,6,4,9,2]
-    values2 = [9,8,7,6,4,7,8,6,4,9,2,10]
-    return render_template('charts3.html',title='chart',labels= labels, values= values, values2= values2)
+@app.route('/chartjs')
+def chartjs():
+    dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx')
+    df = pd.DataFrame(dataxls)
+    #df.fillna(0)
+    temperature = []
+    consumption = []
+    time = []
+    for iter1, iter2 in df.iterrows():
+        temperature.append(iter2.out_door_temp)
+        consumption.append(iter2.electricity_usage)
+        temptime = iter2.time 
+        temptime = temptime.ctime()
+        time.append(temptime)
+    labels = [01-05-12, 30-04-12,
+27-04-12,26-04-12,
+25-04-12, 24-04-12,
+23-04-12,20-04-12,
+19-04-12, 18-04-12,
+17-04-12, 16-04-12]
+    values = [1,10,9,8,2,6,4,7,8,6,4,9]
+    values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
+    return render_template('chartjs1.html',title='chartjs',time=time,labels= labels, values= values, values2= values2)
 
 @app.route('/linechart')
 def linechart():
+    dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx') 
+    df = pd.DataFrame(dataxls)
+    #df.fillna(0)
+    temperature = []
+    consumption = []
+    time = []
+    for iter1, iter2 in df.iterrows():
+        temperature.append(iter2.out_door_temp)
+        consumption.append(iter2.electricity_usage)
+        temptime = iter2.time 
+        temptime = temptime.ctime()
+        time.append(temptime) 
+    labels = [1-05-12, 30-04-12,
+27-04-12,26-04-12,
+25-04-12, 24-04-12,
+23-04-12,20-04-12,
+19-04-12, 18-04-12,
+17-04-12, 16-04-12]
+    values = [0,10,9,8,2,6,4,7,8,6,4,9]
+    values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
+    return render_template('linechart.html',temperature= temperature[10000:],consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
+
+
+@app.route('/zoomablelinechart')
+def zoomablelinechart():
     dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx') 
     df = pd.DataFrame(dataxls)
     df.fillna(0)
@@ -58,5 +92,5 @@ def linechart():
 17-04-12, 16-04-12]
     values = [0,10,9,8,2,6,4,7,8,6,4,9]
     values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
-    return render_template('linechart.html',temperature= temperature,consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
+    return render_template('zoomablelinechart.html',temperature= temperature,consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
 
