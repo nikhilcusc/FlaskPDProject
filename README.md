bf093657de1b5c490ef4cf6544ecce719bf03570
nikhilcusc <nikhilc@usc.edu>
2019-07-28 16:28:28 -0700

    removed second update function
    integrated both temperature and consumption in one chart and made cosmetic changes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # FlaskPDProject
+Loads the data from PD_challange_dataset.xlsx. Plots temperature and consumption on top of each other. 
+The temperature has a background fill so it is easier to differentiate between temperature and consumption.
+
+The Slider allows to change number of steps, start index and end index. 
+
+Because we have so many datapoints, it is advisable to update it as soon as the page has loaded.  
+Here is a table of my loading times (in seconds):
+Browser->|Firefox | Chrome 
+------------ | -------------|---------------
+Run 1 | 301.27 | 51.94
+Run 2 | 301.80 | 48.69
+Average | 301.535 | 50.315
+
+To run Flask quickly, I have a .sh file called runFlask.sh
--- Sol/websw.py
+++ Sol/websw.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template, Markup
 import pandas as pd
-
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html',title='About')
-@app.route('/signup')
-def signup():
-    #form = registrationForm()
-    return render_template('signup.html',title='signup')
 
-@app.route('/login')
-def login():
-    #form = loginForm()
-    return render_template('login.html',title='login')
 @app.route('/chartjs')
 def chartjs():
     dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx')
@@ -35,62 +21,5 @@ def chartjs():
         temptime = iter2.time 
         temptime = temptime.ctime()
         time.append(temptime)
-    labels = [01-05-12, 30-04-12,
-27-04-12,26-04-12,
-25-04-12, 24-04-12,
-23-04-12,20-04-12,
-19-04-12, 18-04-12,
-17-04-12, 16-04-12]
-    values = [1,10,9,8,2,6,4,7,8,6,4,9]
-    values2 = [0,9,8,2,6,4,7,8,6,4]
+    
     return render_template('chartjs1.html',title='chartjs',time= time, consumption= consumption, temperature= temperature)
-
-@app.route('/linechart')
-def linechart():
-    dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx') 
-    df = pd.DataFrame(dataxls)
-    #df.fillna(0)
-    temperature = []
-    consumption = []
-    time = []
-    for iter1, iter2 in df.iterrows():
-        temperature.append(iter2.out_door_temp)
-        consumption.append(iter2.electricity_usage)
-        temptime = iter2.time 
-        temptime = temptime.ctime()
-        time.append(temptime) 
-    labels = [1-05-12, 30-04-12,
-27-04-12,26-04-12,
-25-04-12, 24-04-12,
-23-04-12,20-04-12,
-19-04-12, 18-04-12,
-17-04-12, 16-04-12]
-    values = [0,10,9,8,2,6,4,7,8,6,4,9]
-    values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
-    return render_template('linechart.html',temperature= temperature[10000:],consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
-
-
-@app.route('/zoomablelinechart')
-def zoomablelinechart():
-    dataxls = pd.read_excel (r'/home/fortknox/Documents/PD/PD_challenge/PD_challange_dataset.xlsx') 
-    df = pd.DataFrame(dataxls)
-    df.fillna(0)
-    temperature = []
-    consumption = []
-    time = []
-    for iter1, iter2 in df.iterrows():
-        temperature.append(iter2.out_door_temp)
-        consumption.append(iter2.electricity_usage)
-        temptime = iter2.time 
-        temptime = temptime.ctime()
-        time.append(temptime) 
-    labels = [1-05-12, 30-04-12,
-27-04-12,26-04-12,
-25-04-12, 24-04-12,
-23-04-12,20-04-12,
-19-04-12, 18-04-12,
-17-04-12, 16-04-12]
-    values = [0,10,9,8,2,6,4,7,8,6,4,9]
-    values2 = [0,9,8,2,6,4,7,8,6,4,9,2]
-    return render_template('zoomablelinechart.html',temperature= temperature,consumption= consumption, time= time, title='chart',labels= labels, values= values, values2= values2)
-
